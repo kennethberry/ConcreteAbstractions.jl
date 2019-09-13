85bff2c4ed57fb9dadd1cf394fd00f6850b9648b
Kenneth Berry <rosskberry@gmail.com>
2019-09-13 14:29:57 -0600

    Update to be compatible with Julia v1.0.0
    Change all occurrences of `type` keyword to `struct` and `abstract` to `abstract type`.
    
    Add auto-generated Project.toml.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,22 @@
 using ConcreteAbstractions
-using Base.Test
+using Test
 
-@base type AbstractFoo{T}
+@base struct AbstractFoo{T}
     a
     b::Int
     c::T
     d::Vector{T}
 end
 
-@extend type Foo <: AbstractFoo
+@extend struct Foo <: AbstractFoo
     e::T
 end
 
 @testset begin
     @test Foo <: AbstractFoo
-
     foo = Foo(10,10,10,[10],5)
     @test isa(foo, Foo{Int})
-    @test fieldnames(foo) == [:a,:b,:c,:d,:e]
+    @test fieldnames(typeof(foo)) == (:a,:b,:c,:d,:e)
     @test isa(foo.a, Int)
     @test isa(foo.b, Int)
     @test isa(foo.c, Int)
